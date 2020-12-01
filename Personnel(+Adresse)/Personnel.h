0dcf6679bbfe65bc35cb4109c063d9be6413d833
IWillBecomeAGod <75016647+IWillBecomeAGod@users.noreply.github.com>
2020-12-01 21:01:52 +0100

    Create Personnel.h

---
+++ Personnel(+Adresse)/Personnel.h
@@ -0,0 +1,36 @@
+#pragma once
+
+using namespace System;
+namespace NS_Composants
+{
+	ref class CL_map_TBPERSONNE
+	{
+	private:
+		int IDPersonnel;
+		int IDAdresse;
+		int IDSuperieur;
+		String^ nomPersonnel;
+		String^ prenomPersonnel;
+		String^ DateEmbauche;
+
+	public:
+		CL_map_TBPERSONNE(void);
+		String^ SELECT(void);
+		String^ INSERT(void);
+		String^ UPDATE(void);
+		String^ DELETE(void);
+		void setIDPersonnel(int);
+		void setIDAdresse(int);
+		void setIDSuperieur(int);
+		void setPrenomPersonnel(String^);
+		void setNomPersonnel(String^);
+		void setDateEmbauche(String^);
+		int getIDPersonnel(void);
+		int getIDAdresse(void);
+		int getIDSuperieur(void);
+		String^ getNomPersonnel(void);
+		String^ getprenomPersonnel(void);
+		String^ getDateEmbauche(void);
+	};
+}
+
