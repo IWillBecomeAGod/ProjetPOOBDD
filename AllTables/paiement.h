1dadfc7adcd984dcfcb0f342e42b55a647f283b2
IWillBecomeAGod <75016647+IWillBecomeAGod@users.noreply.github.com>
2020-12-05 19:30:45 +0100

    Create paiement.h

---
+++ AllTables/paiement.h
@@ -0,0 +1,34 @@
+#pragma once
+
+using namespace System;
+namespace NS_Composants
+{
+	ref class CL_map_TBPAIEMENT
+	{
+	private:
+		int IDPaiement;
+		int IDCommande;
+		String^ ModePaiement;
+		String^ MontantPaiement;
+		String^ DatePaiement;
+
+	public:
+		CL_map_TBPAIEMENT(void);
+		String^ SELECT(void);
+		String^ INSERT(void);
+		String^ UPDATE(void);
+		String^ DELETE(void);
+		void setIDPaiement(int);
+		void setIDCommande(int);
+		void setModePaiement(String^);
+		void setMontantPaiement(String^);
+		void setDatePaiement(String^);
+		int getIDPaiement(void);
+		int getIDCommande(void);
+		String^ getModePaiement(void);
+		String^ getMontantPaiement(void);
+		String^ getDatePaiement(void);
+	};
+}
+
+
