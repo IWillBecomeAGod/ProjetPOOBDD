3919a42e6fbfae6691bb0c34848fd90da34c33e6
IWillBecomeAGod <75016647+IWillBecomeAGod@users.noreply.github.com>
2020-12-08 01:48:46 +0100

    Create CL_CAD.h

---
+++ Finalversion/CL_CAD.h
@@ -0,0 +1,28 @@
+#pragma once
+
+using namespace System;
+using namespace System::Data;
+using namespace System::Xml;
+using namespace MySql::Data::MySqlClient;
+
+namespace NS_Composants
+{
+	ref class CL_CAD
+	{
+	private:
+		String^ cnx;
+		String^ rq_sql;
+		MySqlConnection^ CNX;
+		MySqlCommand^ CMD;
+		MySqlDataAdapter^ DA;
+		DataSet^ DS;
+		DataTable^ DT;
+		void setSQL(String^);
+	public:
+		CL_CAD(void);
+		int actionRowsID(String^);
+		void actionRows(String^);
+		DataSet^ getRows(String^, String^);
+		DataTable^ getRowsAfficher(String^);
+	};
+}
